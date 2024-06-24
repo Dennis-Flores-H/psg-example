9a49f46f8749e0b1147a7f7233d554dd645a59db
Dennis Flores Huanca <dfloresh2@fcpn.edu.bo>
2024-06-24 14:24:01 -0400

    Juego de mesa favorito

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,9 @@
+print("Reglas del Sudoku Clásico:")
+print("- Llena el tablero 9x9 con números del 1 al 9.")
+print("- Cada fila, columna y región 3x3 debe contener todos los números del 1 al 9 sin repetirse.")
+print("- Al inicio, algunas casillas ya tienen números predefinidos como pistas.")
+print("- Usa lógica y razonamiento deductivo para resolver el puzzle.")
+print("- No se permite adivinanza ni prueba y error para completar el sudoku.")
+print("- Un sudoku se completa correctamente cuando todas las casillas están llenas")
+print("  según las reglas mencionadas.")
+
