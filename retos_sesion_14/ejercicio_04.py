457e3ca0f2060d0b499826899664448678f97c59
Dennis Flores Huanca <dfloresh2@fcpn.edu.bo>
2024-07-21 18:40:04 -0400

    Correcion: valor de Pi estatico

--- retos_sesion_14/ejercicio_04.py
+++ retos_sesion_14/ejercicio_04.py
@@ -1,12 +1,14 @@
-import math
+# Correccion: Declaramos el valor de pi de manera estática
+pi = 3.141592653589793
 
-# Crear la función anónima
-area_circulo = lambda radio: math.pi * radio**2
+# Creamos la función anónima
+area_circulo = lambda radio: pi * radio**2
 
-# Calcular el área del círculo con radio 5
+# Calculamos el área del círculo con radio 5
 radio = 5
 resultado = area_circulo(radio)
-# Redondear el área a 3 decimales usando format
+
+# Redondeamos el área a 3 decimales usando format
 resultado = "{:.3f}".format(resultado)
 
 print(f"El área de un círculo con radio {radio} es: {resultado}")
